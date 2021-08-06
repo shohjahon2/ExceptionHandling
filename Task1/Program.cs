0af0fd7024c22dfbccaf23b5c1c164d08ef25f8e
User <User@DESKTOP-M9Q3269>
2021-08-06 16:22:46 +0500

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Task2;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string enteredString = Console.ReadLine();
+            try
+            {
+                Console.WriteLine(enteredString[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            int response = 0;
+            string trimmedString = stringValue.Replace(" ","");
+            trimmedString = trimmedString.Replace("+","");
+
+            if (!trimmedString.StartsWith('-'))
+            {
+                foreach (char c in trimmedString)
+                {
+                    response *= 10;
+                    response += c - '0';
+                }
+            }
+            else
+            {
+                trimmedString = trimmedString.Replace("-","");
+                foreach (char c in trimmedString)
+                {
+                    response *= 10;
+                    response += c - '0';
+                }
+                response = -response;
+
+            }
+            return response;
+        }
+    }
+}
